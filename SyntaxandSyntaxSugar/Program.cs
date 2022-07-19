be4e47f06c5e0a9d6a32cf8844415083fe198aef
Alexander Ondrachek <ondrachek@gmail.com>
2022-07-18 20:28:09 -0500

    initial commit

---
+++ SyntaxandSyntaxSugar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp Syntax and Syntax Sugar
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        int answer = 4;
+        string response;
+
+        if (answer < 9)
+        {
+            response = answer + " is less than nine";
+        }
+        else
+        {
+            response = answer + "greater than or equal to nine";
+        }
+        var answer = 4;
+        var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than opr equal to nine"; 
+    }
+    }
+}
